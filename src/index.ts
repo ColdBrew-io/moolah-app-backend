6567980bafdc75f352961fab75421030824fc985
OskyJr <49112599+OskyJr@users.noreply.github.com>
2021-12-26 19:48:42 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,13 @@ import cors from 'cors';
 dotenv.config();
 const app = express();
 
-app.use(cors());
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const connection = mysql.createPool({
