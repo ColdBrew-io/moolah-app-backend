997b904b896e84704931549bb98355a7b3362f48
OskyJr <49112599+OskyJr@users.noreply.github.com>
2021-12-31 12:18:35 +0800

    Develop (#2)
    * fix post request for localhost, actual post still being blocked by cors
    
    * Update Budgets.ts
    
    * removed hard coded index from post request

--- src/routes/Budgets.ts
+++ src/routes/Budgets.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from 'express';
 const router = express.Router();
-import mysql from 'mysql';
 import { connection } from './../index';
 
 // get all
@@ -23,16 +22,19 @@ router.get('/', (req: Request, res: Response) => {
 
 // single post (from summary page for now)
 router.post('/', (req: Request, res: Response) => {
-
     const budgetType = req.body.budget_type;
     const budgetAmount = req.body.budget_amount;
     const savingGoal = req.body.saving_goal;
     const repeatBudget = req.body.repeat_budget;
 
-    const insertQuery = 'INSERT INTO budgets (budget_type, budget_amount, saving_goal, repeat_budget) VALUES (?,?,?,?)'
+    let insertQuery = `INSERT INTO budgets (budget_type, budget_amount, saving_goal, repeat_budget) `;
+    insertQuery += `VALUES ("${budgetType}", ${budgetAmount}, ${savingGoal}, ${repeatBudget})`;
+
     console.log(insertQuery)
-    connection.query(insertQuery, [budgetType, budgetAmount, savingGoal, repeatBudget], (err, result) => {
+    connection.query(insertQuery, (err, result) => {
         console.log(result);
+        console.log("err", err);
+        res.send(result);
     });
 });
 
